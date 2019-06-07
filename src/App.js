f0d438788590d3e6cfae56c57f4601a2675feb3a
mohitshah01 <shahmohitm@gmail.com>
2019-06-07 15:16:32 +0530

    property desktop app in electron and react

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter as Router, Switch, HashRouter } from 'react-router-dom';
+// import Header from './Header/Header';
+// import Footer from './Footer/Footer';
+import Agents from './Agents/Agents';
+import Contact from './Contact/Contact';
+import PropertySearch from './PropertySearch/PropertySearch';
+import HomePage from './HomePage/HomePage'
+import PropertySingle from './PropertySingle/PropertySingle';
+import AddProperty from './AddProperty/AddProperty';
+import Login from './Admin/Login';
+import { platform } from 'os';
+import shell from 'shelljs';
+const { app } = require('electron').remote
+const path = require('path');
+
+
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {};
+		shell.mkdir('-p',path.join(__dirname, app.getPath('pictures')+'/propertyDesktopApp/property/'))
+	}
+
+	componentDidMount(){
+		console.log(`This platform is ${path.join(__dirname, '/home/rao')}`);
+		console.log(platform(), app.getPath('pictures'));
+	}
+	render(){
+		return (
+			<Router>
+				<HashRouter>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/admin" component={Login} />
+					<Route path="/home" component={HomePage} />
+					<Route path="/add-property" component={AddProperty} />
+					<Route path="/agents" component={Agents} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/property-search" component={PropertySearch} />
+					<Route path="/property-single/:id" component={PropertySingle} />
+					<Route path="/edit-property/:id" component={AddProperty} />
+				</Switch>
+				</HashRouter>
+			</Router>
+			);
+		}
+	}
+	
+	export default App;
---
+++ src/config.js
@@ -0,0 +1,14 @@
+// const baseApiUrl = "http://132.140.160.60:4000";
+// const baseMediaUrl = "http://132.140.160.60/rojkind/server/uploads/";
+
+const { app } = require('electron').remote
+
+
+const baseApiUrl = "http://ec2-18-218-26-118.us-east-2.compute.amazonaws.com:4000";
+const uploadPath = app.getPath('pictures')+'/propertyDesktopApp/property/'
+const baseMediaUrl = "file://"+app.getPath('pictures')+'/propertyDesktopApp/property/'//"https://s3.us-west-1.amazonaws.com/assets.rojkind.com.mx/"//app.getPath('Pictures');
+export const config = {
+    baseApiUrl: baseApiUrl,
+    uploadpath: uploadPath,
+    baseMediaUrl: baseMediaUrl
+};
